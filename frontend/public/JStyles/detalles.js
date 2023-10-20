68f7585506acd4db0ea9c6fe981288a2f1688175
BryzonSnow <bairon.sanhueza2001@alumnos.ubiobio.cl>
2023-10-19 21:18:39 -0300

    Subida de archivos para front-end

---
+++ frontend/public/JStyles/detalles.js
@@ -0,0 +1,32 @@
+//Funcion de click
+document.getElementById("btn_open").addEventListener("click", open_close_menu);
+//Variables
+
+var side_menu = document.getElementById("menu__side");
+var btn_open = document.getElementById("btn_open");
+var body = document.getElementById("body");
+
+function open_close_menu() {
+    body.classList.toggle("body_move");
+    side_menu.classList.toggle("menu__side_move");
+}
+
+//oculta el menu al recargar si la pagina es chica
+
+if (window.innerWidth < 760) {
+    body.classList.add("body_move");
+    side_menu.classList.add("menu__side_move");
+}
+
+//menu adaptable
+
+window.addEventListener("resize", function () {
+    if (this.window.innerWidth > 760) {
+        body.classList.remove("body_move");
+        side_menu.classList.remove("menu__side_move");
+    }
+    if (this.window.innerWidth < 760) {
+        body.classList.remove("body_move");
+        side_menu.classList.remove("menu__side_move");
+    }
+});
