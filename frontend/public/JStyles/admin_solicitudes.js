06a14e2376416d921d07d5a340ec58e777544cd8
BryzonSnow <bairon.sanhueza2001@alumnos.ubiobio.cl>
2023-11-23 21:58:52 -0300

    Acomodo de solicitar y nuevo boton

--- frontend/public/JStyles/admin_solicitudes.js
+++ frontend/public/JStyles/admin_solicitudes.js
@@ -2,30 +2,29 @@
 
 // Función para cargar los datos en la tabla desde el servidor
 async function cargarDatosEnTablaDesdeServidor() {
-    try {
-      // Hacer la solicitud al servidor para obtener las solicitudes
-      const response = await fetch('api/user-home/getSolicitudes', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        // Obtener los datos de respuesta en formato JSON
-        const solicitudes = await response.json();
-  console.log('Solicitudes obtenidas desde el servidor:', solicitudes);
-        // Obtén la referencia a la tabla en el DOM
-        const table = document.querySelector("table tbody");
-  
-        // Limpia la tabla antes de cargar nuevos datos
-        table.innerHTML = '';
-  
-        // Itera sobre las solicitudes y crea las filas de la tabla con el checkbox
-        solicitudes.forEach((solicitud) => {
-          const row = document.createElement("tr");
-          row.innerHTML = `
-              <td><input type="checkbox"></td>
+  try {
+    // Hacer la solicitud al servidor para obtener las solicitudes
+    const response = await fetch('api/user-home/getSolicitudes', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      // Obtener los datos de respuesta en formato JSON
+      const solicitudes = await response.json();
+      console.log('Solicitudes obtenidas desde el servidor:', solicitudes);
+      // Obtén la referencia a la tabla en el DOM
+      const table = document.querySelector("table tbody");
+
+      // Limpia la tabla antes de cargar nuevos datos
+      table.innerHTML = '';
+
+      // Itera sobre las solicitudes y crea las filas de la tabla con el checkbox
+      solicitudes.forEach((solicitud) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
               <td>${solicitud.fecha}</td>
               <td>${solicitud.hora}</td>
               <td>${solicitud.nombre}</td>
@@ -34,31 +33,32 @@ async function cargarDatosEnTablaDesdeServidor() {
               <td>${solicitud.carrera}</td>
               <td>${solicitud.equipo}</td>
               <td>
-                  <button class="sancionar">Sancionar</button>
-                  <button class="observacion">Agregar Observación</button>
+              <button class="sancionar">Sancionar</button>
+              <button class="observacion">Agregar Observación</button>
               </td>
+              <td><input type="checkbox"></td>
           `;
-  
-          table.appendChild(row);
-        });
-      } else {
-        console.error('Error al obtener las solicitudes desde el servidor');
-      }
-    } catch (error) {
-      console.error('Error de red al obtener las solicitudes:', error);
+
+        table.appendChild(row);
+      });
+    } else {
+      console.error('Error al obtener las solicitudes desde el servidor');
     }
+  } catch (error) {
+    console.error('Error de red al obtener las solicitudes:', error);
   }
-  
-  // Agrega un evento de clic al botón "Aceptar"
-  document.getElementById("acceptButton").addEventListener("click", function () {
-    // Lógica de clic en el botón Aceptar, si es necesario
-    // ...
-  
-    // Después de realizar la lógica, vuelve a cargar los datos en la tabla
-    cargarDatosEnTablaDesdeServidor();
-  });
-  
-  // Llama a la función de carga al cargar la página
-  document.addEventListener("DOMContentLoaded", function () {
-    cargarDatosEnTablaDesdeServidor();
-  });
+}
+
+// Agrega un evento de clic al botón "Aceptar"
+document.getElementById("acceptButton").addEventListener("click", function () {
+  // Lógica de clic en el botón Aceptar, si es necesario
+  // ...
+
+  // Después de realizar la lógica, vuelve a cargar los datos en la tabla
+  cargarDatosEnTablaDesdeServidor();
+});
+
+// Llama a la función de carga al cargar la página
+document.addEventListener("DOMContentLoaded", function () {
+  cargarDatosEnTablaDesdeServidor();
+});
